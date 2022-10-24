90380c8fa19bd87deed241cc257037064abca075
Felix <barosiofelix@gmail.com>
2022-10-24 07:18:55 +0300

    Completed Assignment

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,14 @@
 class Person
   # your code here
+  def initialize(args)
+
+    args.each do |key, value|
+      
+      self.class.attr_accessor(key)
+      self.send("#{key}=", args[key])
+
+    end
+
+  end
+
 end
